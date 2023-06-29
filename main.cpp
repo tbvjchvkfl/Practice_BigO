20283bcf6606180fe07fb6cb15271733ecf09977
tbvjchvkfl <anfmdehdnjs@gmail.com>
2023-06-30 00:12:15 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int* Ex4(int Array[], int count);
+
+int main()
+{
+	
+}
+
+int* Ex4(int array[], int count)
+{
+	// pNew라는 이름의 포인터 변수 선언
+	// 해당 변수에 배열의 형태로 매게 변수 count의 값 만큼 int 타입의 변수를 선언함. -> 선언된 변수는 n개
+	int* pNew = new int[count];
+
+	// 매게 변수 i를 위 함수 포인터의 매게 변수 int count의 값보다 작게 반복해서 더해줌.
+	// int 타입의 변수가 1개 선언됨.   선언된 변수 n +1
+	for (int i = 0; i < count; ++i) 
+	{
+		pNew[i] = array[i];
+	}
+
+	delete[] pNew;
+
+
+	return 0;
+}
